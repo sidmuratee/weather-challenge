0839542f39e2e905daa3d57b09bb4ec3a6c63a0a
smurtee <muratees@yahoo.com>
2022-09-13 22:12:48 -0500

    search bar works

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
-let apiKey = '20e32e4eb83733727d2be1bce18fc81a'
+let apiKey = '2010b8ae4ce65d9eb3560a2ab9904413'
+// let cityName = 
+let searchInput = document.querySelector("#searchInput")
+let searchForm = document.querySelector("#searchForm")
+let button = document.querySelector("#button")
 
-let cityName = "Dallas";
-fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`)
-.then(function (response) {
+function callOne(userInput){
+let url = `http://api.openweathermap.org/geo/1.0/direct?q=${userInput}&appid=${apiKey}`
+fetch(url)
 
-    return response.json()
+    .then(function (response) {
 
-})
-.then(function (cityData){
+        return response.json()
 
-
-    console.log(cityData[0].lat);
-    console.log(cityData[0].lon);
-
-    return {lat:cityData[0].lat, lon: cityData[0].lon}
-
-})
-.then(function(latLonData){
-    return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latLonData.lat}&lon=${latLonData.lon}&appid=${apiKey}`)
-        .then(function (response) {
-    
-            return response.json()
-    
-        })
-        .then(function (data) {
-    
-            console.log(data);
-    
-    
-        })
+    })
+    .then(function (cityData) {
 
 
+        console.log(cityData[0].lat);
+        console.log(cityData[0].lon);
+        return { lat: cityData[0].lat, lon: cityData[0].lon };
+    })
+    .then(function (latLonData) {
+        return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latLonData.lat}&lon=${latLonData.lon}&units=imperial&appid=${apiKey}`)
+            .then(function (response) {
+                return response.json()
 
-})
+            })
+            .then(function (weatherData) {
+                console.log(weatherData);
+
+            })
+    })
+}
+
+function handleFormSubmit(event) {
+    event.preventDefault()
+    let userInput = searchInput.value.trim()
+    console.log(userInput)
+    callOne(userInput);
+}
+
+searchForm.addEventListener( "submit" ,handleFormSubmit)
+
+    // function currentWeather(weatherData)
