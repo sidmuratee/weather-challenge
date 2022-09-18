ec4622c3b534d3c4c887391c9bb08c9ac08f43eb
smurtee <muratees@yahoo.com>
2022-09-18 16:37:46 -0500

    current weather and forecast=good

--- script.js
+++ script.js
@@ -16,29 +16,140 @@ function getCurrent(lat, lon) {
             let h2 = document.createElement('h2');
             h2.textContent = weatherData.name;
 
-            let pTemp = document.createElement('p');
-            pTemp.textContent = 'Temperature: ' + weatherData.main.temp;
+            let temp = document.getElementById('temp');
+            temp.textContent = 'Temperature: ' + weatherData.main.temp;
 
-            let pWind = document.createElement('p');
-            pWind.textContent = `Wind: ${weatherData.wind.speed}`
-            
-            let pHumid = document.createElement('p');
-            pHumid.textContent = `Humidity: ${weatherData.main.humidity}`
+            let wind = document.getElementById('wind');
+            wind.textContent = `Wind: ${weatherData.wind.speed}`
+
+            let humid = document.getElementById('humid');
+            humid.textContent = `Humidity: ${weatherData.main.humidity}`
 
             currentDiv.appendChild(h2);
-            currentDiv.appendChild(pTemp);
-            currentDiv.appendChild(pHumid);
-            currentDiv.appendChild(pWind);
+            currentDiv.appendChild(temp);
+            currentDiv.appendChild(humid);
+            currentDiv.appendChild(wind);
+            // let card1Date = document.querySelector('.card1Date')
+            // let temp1 = document.querySelector('#temp1')
+            // let wind1 = document.querySelector('#wind1')
+            // let humid1 = document.querySelector('#humidity1')
 
+            // let card5Date = document.querySelector('.card5Date')
+            // let temp5 = document.querySelector('#temp5')
+            // let wind5 = document.querySelector('#wind5')
+            // let humid5 = document.querySelector('#humidity5')
+            // card5Date.textContent = weatherData.dt_txt;
+            // temp5.textContent = weatherData.main.temp
+            // wind5.textContent = weatherData.wind.speed
+            // humid5.textContent = weatherData.main.humidity
             getForecast(lat, lon)
         })
 }
 
+// function getForecast(lat, lon) {
+
+//     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
+//         .then(function (response) {
+//             return response.json()
+//         })
+//         .then(function (weatherData) {
+//             console.log(weatherData);
+
+//             for (var i = 0; i < weatherData.list.length; i++) {
+//                 if (weatherData.list[i].dt_txt.includes('12:00:00')) {
+//                     console.log(weatherData.list[i])
+//                     let forecastData = weatherData.list[i];
+//                     card1Date.textContent = forecastData.dt_txt;
+//                     temp1.textContent = forecastData.main.temp
+//                     wind1.textContent = forecastData.wind.speed
+//                     humid1.textContent = forecastData.main.humidity
+
+//                 }
+//             }
+//         })
+// }
+
+
+// function getForecast(lat, lon) {
+//     let card2Date = document.querySelector('.card2Date')
+//     let temp2 = document.querySelector('#temp2')
+//     let wind2 = document.querySelector('#wind2')
+//     let humid2 = document.querySelector('#humidity2')
+
+//     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
+//         .then(function (response) {
+//             return response.json()
+
+//         })
+//         .then(function (weatherData) {
+//             console.log(weatherData);
+//             for (var i = 0; i < weatherData.list.length; i++) {
+//                 if (weatherData.list[i].dt_txt.includes('12:00:00')) {
+//                     console.log(weatherData.list[2])
+//                     let forecastData = weatherData.list[2];
+//                     card2Date.textContent = forecastData.dt_txt;
+//                     temp2.textContent = forecastData.main.temp
+//                     wind2.textContent = forecastData.wind.speed
+//                     humid2.textContent = forecastData.main.humidity
+//                 }
+//             }
+//         })
+// }
+// function getForecast(lat, lon) {
+//     let card3Date = document.querySelector('.card3Date')
+//     let temp3 = document.querySelector('#temp3')
+//     let wind3 = document.querySelector('#wind3')
+//     let humid3 = document.querySelector('#humidity3')
+
+//     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
+//         .then(function (response) {
+//             return response.json()
+
+//         })
+//         .then(function (weatherData) {
+//             console.log(weatherData);
+//             for (var i = 0; i < weatherData.list.length; i++) {
+//                 if (weatherData.list[i].dt_txt.includes('12:00:00')) {
+//                     console.log(weatherData.list[i])
+//                     let forecastData = weatherData.list[i];
+//                     card3Date.textContent = forecastData.dt_txt;
+//                     temp3.textContent = forecastData.main.temp
+//                     wind3.textContent = forecastData.wind.speed
+//                     humid3.textContent = forecastData.main.humidity
+//                 }
+//             }
+//         })
+// }
+// function getForecast(lat, lon) {
+//     const card4Date = document.querySelector('.card4Date')
+//     const temp4 = document.querySelector('#temp4')
+//     const wind4 = document.querySelector('#wind4')
+//     const humid4 = document.querySelector('#humidity4')
+
+//     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
+//         .then(function (response) {
+//             return response.json()
+
+//         })
+//         .then(function (weatherData) {
+//             console.log(weatherData);
+//             for (var i = 0; i < weatherData.list.length; i++) {
+//                 if (weatherData.list[i].dt_txt.includes('12:00:00')) {
+//                     console.log(weatherData.list[i])
+//                     let forecastData = weatherData.list[i];
+//                     card4Date.textContent = forecastData.dt_txt;
+//                     temp4.textContent = forecastData.main.temp
+//                     wind4.textContent = forecastData.wind.speed
+//                     humid4.textContent = forecastData.main.humidity
+//                 }
+//             }
+//         })
+// }
 function getForecast(lat, lon) {
-    const card1Date = document.querySelector('.card1Date')
-    const temp1 = document.querySelector('#temp1')
-    const wind1 = document.querySelector('#wind1')
-    const humid1 = document.querySelector('#humidity1')
+    // const card5Date = document.querySelector('.card5Date')
+    // const temp5 = document.querySelector('#temp5')
+    // const wind5 = document.querySelector('#wind5')
+    // const humid5 = document.querySelector('#humidity5')
 
     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
         .then(function (response) {
@@ -50,117 +161,60 @@ function getForecast(lat, lon) {
             for (var i = 0; i < weatherData.list.length; i++) {
                 if (weatherData.list[i].dt_txt.includes('12:00:00')) {
                     console.log(weatherData.list[i])
-                    let forecastData = weatherData.list[i];
-                    card1Date.textContent = forecastData.dt_txt;
-                    temp1.textContent = forecastData.main.temp
-                    wind1.textContent = forecastData.wind.speed
-                    humid1.textContent = forecastData.main.humidity
+                   
+                    let forecastData1 = weatherData.list[7];
+                    let card1Date = document.querySelector('.card1Date')
+                    let temp1 = document.querySelector('#temp1')
+                    let wind1 = document.querySelector('#wind1')
+                    let humid1 = document.querySelector('#humidity1')
+                    card1Date.textContent = forecastData1.dt_txt;
+                    temp1.textContent = forecastData1.main.temp
+                    wind1.textContent = forecastData1.wind.speed
+                    humid1.textContent = forecastData1.main.humidity
+
+                    let forecastData2 = weatherData.list[15];
+                    let card2Date = document.querySelector('.card2Date')
+                    let temp2 = document.querySelector('#temp2')
+                    let wind2 = document.querySelector('#wind2')
+                    let humid2 = document.querySelector('#humidity2')
+                    card2Date.textContent = forecastData2.dt_txt;
+                    temp2.textContent = forecastData2.main.temp
+                    wind2.textContent = forecastData2.wind.speed
+                    humid2.textContent = forecastData2.main.humidity
+
+                    let forecastData3 = weatherData.list[23];
+                    let card3Date = document.querySelector('.card3Date')
+                    let temp3 = document.querySelector('#temp3')
+                    let wind3 = document.querySelector('#wind3')
+                    let humid3 = document.querySelector('#humidity3')
+                    card3Date.textContent = forecastData3.dt_txt;
+                    temp3.textContent = forecastData3.main.temp
+                    wind3.textContent = forecastData3.wind.speed
+                    humid3.textContent = forecastData3.main.humidity
+
+                    let forecastData4 = weatherData.list[31];
+                    let card4Date = document.querySelector('.card4Date')
+                    let temp4 = document.querySelector('#temp4')
+                    let wind4 = document.querySelector('#wind4')
+                    let humid4 = document.querySelector('#humidity4')
+                    card4Date.textContent = forecastData4.dt_txt;
+                    temp4.textContent = forecastData4.main.temp
+                    wind4.textContent = forecastData4.wind.speed
+                    humid4.textContent = forecastData4.main.humidity
+
+                    let forecastData5 = weatherData.list[39];
+                    let card5Date = document.querySelector('.card5Date')
+                    let temp5 = document.querySelector('#temp5')
+                    let wind5 = document.querySelector('#wind5')
+                    let humid5 = document.querySelector('#humidity5')
+                    card5Date.textContent = forecastData5.dt_txt;
+                    temp5.textContent = forecastData5.main.temp
+                    wind5.textContent = forecastData5.wind.speed
+                    humid5.textContent = forecastData5.main.humidity
                 }
             }
         })
 }
-
-
-// function getForecast(lat, lon) {
-//     const card2Date = document.querySelector('.card2Date')
-//     const temp2 = document.querySelector('#temp2')
-//     const wind2 = document.querySelector('#wind2')
-//     const humid2 = document.querySelector('#humidity2')
-
-//     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
-//         .then(function (response) {
-//             return response.json()
-
-//         })
-//         .then(function (weatherData) {
-//             console.log(weatherData);
-//             for (var i = 0; i < weatherData.list.length; i++) {
-//                 if (weatherData.list[i].dt_txt.includes('12:00:00')) {
-//                     console.log(weatherData.list[i])
-//                     let forecastData = weatherData.list[i];
-//                     card2Date.textContent = forecastData.dt_txt;
-//                     temp2.textContent = forecastData.main.temp
-//                     wind2.textContent = forecastData.wind.speed
-//                     humid2.textContent = forecastData.main.humidity
-//                 }
-//             }
-//         })
-// }
-// function getForecast(lat, lon) {
-//     const card1Date = document.querySelector('.card1Date')
-//     const temp1 = document.querySelector('#temp1')
-//     const wind1 = document.querySelector('#wind1')
-//     const humid1 = document.querySelector('#humidity1')
-
-//     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
-//         .then(function (response) {
-//             return response.json()
-
-//         })
-//         .then(function (weatherData) {
-//             console.log(weatherData);
-//             for (var i = 0; i < weatherData.list.length; i++) {
-//                 if (weatherData.list[i].dt_txt.includes('12:00:00')) {
-//                     console.log(weatherData.list[i])
-//                     let forecastData = weatherData.list[i];
-//                     card1Date.textContent = forecastData.dt_txt;
-//                     temp1.textContent = forecastData.main.temp
-//                     wind1.textContent = forecastData.wind.speed
-//                     humid1.textContent = forecastData.main.humidity
-//                 }
-//             }
-//         })
-// }
-// function getForecast(lat, lon) {
-//     const card1Date = document.querySelector('.card1Date')
-//     const temp1 = document.querySelector('#temp1')
-//     const wind1 = document.querySelector('#wind1')
-//     const humid1 = document.querySelector('#humidity1')
-
-//     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
-//         .then(function (response) {
-//             return response.json()
-
-//         })
-//         .then(function (weatherData) {
-//             console.log(weatherData);
-//             for (var i = 0; i < weatherData.list.length; i++) {
-//                 if (weatherData.list[i].dt_txt.includes('12:00:00')) {
-//                     console.log(weatherData.list[i])
-//                     let forecastData = weatherData.list[i];
-//                     card1Date.textContent = forecastData.dt_txt;
-//                     temp1.textContent = forecastData.main.temp
-//                     wind1.textContent = forecastData.wind.speed
-//                     humid1.textContent = forecastData.main.humidity
-//                 }
-//             }
-//         })
-// }
-// function getForecast(lat, lon) {
-//     const card1Date = document.querySelector('.card1Date')
-//     const temp1 = document.querySelector('#temp1')
-//     const wind1 = document.querySelector('#wind1')
-//     const humid1 = document.querySelector('#humidity1')
-
-//     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
-//         .then(function (response) {
-//             return response.json()
-
-//         })
-//         .then(function (weatherData) {
-//             console.log(weatherData);
-//             for (var i = 0; i < weatherData.list.length; i++) {
-//                 if (weatherData.list[i].dt_txt.includes('12:00:00')) {
-//                     console.log(weatherData.list[i])
-//                     let forecastData = weatherData.list[i];
-//                     card1Date.textContent = forecastData.dt_txt;
-//                     temp1.textContent = forecastData.main.temp
-//                     wind1.textContent = forecastData.wind.speed
-//                     humid1.textContent = forecastData.main.humidity
-//                 }
-//             }
-//         })
-// }
 
 function callOne(userInput) {
     let url = `http://api.openweathermap.org/geo/1.0/direct?q=${userInput}&appid=${apiKey}`
